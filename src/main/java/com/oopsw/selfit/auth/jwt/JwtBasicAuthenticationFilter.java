90a535a98c20dae3d3718cdb0167b77f89cf8ef9
KOMINPYO <gnjgnjgnj1@gmail.com>
2025-06-13 01:49:19 +0900

    refactor: SecuritryContext부분 메소드로 추출

--- src/main/java/com/oopsw/selfit/auth/jwt/JwtBasicAuthenticationFilter.java
+++ src/main/java/com/oopsw/selfit/auth/jwt/JwtBasicAuthenticationFilter.java
@@ -32,12 +32,12 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class JwtBasicAuthenticationFilter extends BasicAuthenticationFilter {
 
-	private MemberRepository memberRepository;
-	private CustomOAuth2UserService customOAuth2UserService;
-	private CustomUserDetailsService customUserDetailsService;
+	private final MemberRepository memberRepository;
+	private final CustomOAuth2UserService customOAuth2UserService;
+	private final CustomUserDetailsService customUserDetailsService;
 
-	public JwtBasicAuthenticationFilter(AuthenticationManager authenticationManager, MemberRepository memberRepository, CustomOAuth2UserService customOAuth2UserService,
-		CustomUserDetailsService customUserDetailsService) {
+	public JwtBasicAuthenticationFilter(AuthenticationManager authenticationManager, MemberRepository memberRepository,
+		CustomOAuth2UserService customOAuth2UserService, CustomUserDetailsService customUserDetailsService) {
 		super(authenticationManager);
 		this.memberRepository = memberRepository;
 		this.customOAuth2UserService = customOAuth2UserService;
@@ -63,39 +63,38 @@ public class JwtBasicAuthenticationFilter extends BasicAuthenticationFilter {
 		String token = jwtToken.replace(JwtProperties.TOKEN_PREFIX, "");
 
 		//3. jwt 서명 확인
-		int memberId = JWT.require(Algorithm.HMAC512(JwtProperties.SECRET)).build()
-			.verify(token).getClaim("memberId").asInt();
+		int memberId = JWT.require(Algorithm.HMAC512(JwtProperties.SECRET))
+			.build()
+			.verify(token)
+			.getClaim("memberId")
+			.asInt();
 
 		//4. 유효한 계정 확인
 		Member member = memberRepository.getMember(memberId);
 		if (member == null) {
-			throw new UsernameNotFoundException("잘못된 토큰입니다.");
+			throw new UsernameNotFoundException("잘못된 계정입니다.");
 		}
+
+		//5. ContextHolder에 저장
+		setSecurityContextHolder(member);
+		chain.doFilter(request, response);
+	}
+
+	private void setSecurityContextHolder(Member member) {
 		Authentication authentication;
 
 		if (member.getMemberType().equals("DEFAULT")) {
 
 			UserDetails userDetails = customUserDetailsService.loadUserByUsername(member.getEmail());
-			authentication = new UsernamePasswordAuthenticationToken(
-				userDetails, null, userDetails.getAuthorities()
-			);
+			authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
 
 		} else {
 			Map<String, Object> attributes = Map.of("email", member.getEmail());
 			CustomOAuth2User oAuth2User = customOAuth2UserService.convertToCustomOAuth2User(attributes);
 
-			authentication = new UsernamePasswordAuthenticationToken(
-				oAuth2User, null, oAuth2User.getAuthorities()
-			);
-
+			authentication = new UsernamePasswordAuthenticationToken(oAuth2User, null, oAuth2User.getAuthorities());
 		}
 
-		HttpSession session = request.getSession(true);
-		session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY,
-			SecurityContextHolder.getContext());
 		SecurityContextHolder.getContext().setAuthentication(authentication);
-
-		chain.doFilter(request, response);
-
 	}
 }
