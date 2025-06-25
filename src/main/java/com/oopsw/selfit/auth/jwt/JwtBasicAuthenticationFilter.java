ca7fdaa042813de9d38f4fd8721cc2f732b5f9e9
KOMINPYO <gnjgnjgnj1@gmail.com>
2025-06-25 23:05:59 +0900

    refactor:  기존 방식으로 회귀(chain.doFilter)
    - permitAll 경로도 이거 실행되서 다시 수정함

--- src/main/java/com/oopsw/selfit/auth/jwt/JwtBasicAuthenticationFilter.java
+++ src/main/java/com/oopsw/selfit/auth/jwt/JwtBasicAuthenticationFilter.java
@@ -3,6 +3,7 @@ package com.oopsw.selfit.auth.jwt;
 //권한 인증 -> header를 기준으로 하고 싶을 때
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.security.authentication.AuthenticationManager;
@@ -30,6 +31,9 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class JwtBasicAuthenticationFilter extends BasicAuthenticationFilter {
 
+	private static final List<String> PUBLIC_PATHS = List.of("/api/board/list", "/api/board/*", "/api/board/comments",
+		"/api/account/member/check-login", "/api/account/member", "/api/dashboard/food/openSearch",
+		"/api/dashboard/exercise/openSearch");
 	private final MemberRepository memberRepository;
 	private final CustomOAuth2UserService customOAuth2UserService;
 	private final CustomUserDetailsService customUserDetailsService;
@@ -54,7 +58,7 @@ public class JwtBasicAuthenticationFilter extends BasicAuthenticationFilter {
 
 		//1. jwt 토큰이 있는지 확인
 		if (jwtToken == null || jwtToken.trim().isEmpty() || !jwtToken.startsWith(JwtProperties.TOKEN_PREFIX)) {
-			response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized: No JWT token found");
+			chain.doFilter(request, response);
 			return;
 		}
 		//2. jwt 토큰
@@ -91,4 +95,5 @@ public class JwtBasicAuthenticationFilter extends BasicAuthenticationFilter {
 		chain.doFilter(request, response);
 
 	}
+
 }
