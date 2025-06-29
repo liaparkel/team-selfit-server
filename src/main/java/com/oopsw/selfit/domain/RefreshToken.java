95e9f04082d5e9afda13915b7d00c273c42c8425
KOMINPYO <gnjgnjgnj1@gmail.com>
2025-06-29 20:17:25 +0900

    feat: RefreshToken 추가

---
+++ src/main/java/com/oopsw/selfit/domain/RefreshToken.java
@@ -0,0 +1,37 @@
+package com.oopsw.selfit.domain;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "refresh_tokens")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RefreshToken {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(nullable = false, length = 512)
+	private String token;
+
+	@Column(unique = true, length = 128)
+	private String jti;
+
+	@Column(nullable = false)
+	private Date expiresAt;
+
+}
