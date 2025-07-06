c2ba59cc23758f2f9f7596768d08717bc76771e8
KOMINPYO <gnjgnjgnj1@gmail.com>
2025-07-06 22:37:45 +0900

    refactor: AccessToken 10분으로 변경

--- src/main/java/com/oopsw/selfit/auth/jwt/JwtProperties.java
+++ src/main/java/com/oopsw/selfit/auth/jwt/JwtProperties.java
@@ -2,7 +2,7 @@ package com.oopsw.selfit.auth.jwt;
 
 public interface JwtProperties {
 	String SECRET = "c0783cea-e162-4965-927d-c19cd0a38791";
-	int TIMEOUT = 1 * 1 * 1000;
+	int TIMEOUT = 10 * 60 * 1000;
 	String TOKEN_PREFIX = "Bearer ";
 	String HEADER_STRING = "selfitKosta";
 
